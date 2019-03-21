9ce469fce4e7c858273e2c0327a8f862845fa4dc
soleilyasmina <mounlasolomon@gmail.com>
2019-03-21 10:58:54 -0400

    big update: - used readline sync - implemented chalk - used try && catch

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 const axios = require('axios');
+const prompt = require('readline-sync');
+const chalk = require('chalk');
 
 const BASE_URL = 'https://git.generalassemb.ly/';
-const COHORT = 'sei-nyc-jeopardy';
-const REPO = process.argv[2];
 
 const main = async () => {
-  const resp = await axios({
-    method: 'get',
-    url: `https://git.generalassemb.ly/api/v3/repos/${COHORT}/${REPO}/pulls`
-  });
-  const submits = resp.data.forEach(item => {
-    let user = item.user.login;
-    console.log(`git clone ${BASE_URL}${user}/${REPO} ${user}`);
-    console.log(`cd ${user} && npm install && cd ..`);
-  });
+  try {
+    console.log(`Welcome to ${chalk.blue('Git Over Here')}!`);
+    const COHORT = prompt.question(`What is the name of your ${chalk.red('cohort')}? \n`);
+    const REPO = prompt.question(`What is the name of your ${chalk.red('repo')}? \n`);
+    console.log('\n');
+    const resp = await axios({
+      method: 'get',
+      url: `https://git.generalassemb.ly/api/v3/repos/${COHORT}/${REPO}/pulls`
+    });
+    const submits = resp.data.forEach(item => {
+      let user = item.user.login;
+      console.log(`git clone ${BASE_URL}${user}/${REPO} ${user}`);
+      console.log(`cd ${user} && npm install && cd ..`);
+    });
+  } catch (e) {
+    let { status } = e.response
+    switch(status) {
+      case 404:
+        console.log('Sorry, this repo was not found!');
+        break;
+      default:
+        console.log('Error.');
+        break;
+    }
+  }
 }
 
 main();
