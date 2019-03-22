0608c1d3c185684a0a7fa94dbbb03bdfa5d2504f
soleilyasmina <mounlasolomon@gmail.com>
2019-03-21 22:11:03 -0400

    folder containing repos now created one directory above git-over-here

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const main = async () => {
       method: 'get',
       url: `https://git.generalassemb.ly/api/v3/repos/${COHORT}/${REPO}/pulls`
     });
-    console.log(`rm -rf ${REPO} && mkdir ${REPO} && cd ${REPO}`);
+    console.log(`cd .. && rm -rf ${REPO} && mkdir ${REPO} && cd ${REPO}`);
     const submits = resp.data.forEach(item => {
       let user = item.user.login;
       let title = item.title.split(' ')[0];
