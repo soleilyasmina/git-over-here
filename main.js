cbd127a1ad24e3cac864fcdbf41fa917964134bf
soleilyasmina <mounlasolomon@gmail.com>
2019-03-24 17:53:48 -0400

    added details about submission number

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ const main = async () => {
       method: 'get',
       url: `https://git.generalassemb.ly/api/v3/repos/${COHORT}/${REPO}/pulls`
     });
-    console.log(`echo ${colorText('Repo found!', 'green')}\n`);
+    console.log(`echo ${colorText('Repo found!', 'green')}`);
+    console.log(`echo ${colorText(`${resp.data.length !== 1 ? resp.data.length + ' submissions' : '1 submission' } pulled.`, 'green')}`);
     console.log(`cd .. && rm -rf ${REPO} && mkdir ${REPO} && cd ${REPO}`);
     const submits = resp.data.forEach(item => {
       let user = item.user.login;
