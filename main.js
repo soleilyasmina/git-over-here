c1a475e828627829f872ff10624661e681d32d3d
soleilyasmina <mounlasolomon@gmail.com>
2019-03-24 14:37:43 -0400

    moved npm question to after get request

--- main.js
+++ main.js
@@ -23,12 +23,12 @@ const main = async () => {
     console.log(`echo Welcome to ${colorText('Git Over Here', 'blue')}!`);
     const COHORT = prompt.question(`What is the name of your GitHub organization? (e.g. sei-nyc-jeopardy)\n`);
     const REPO = prompt.question(`Which repository do you want to pull from? (e.g. js-data-types-homework)\n`);
-    const NPM = prompt.question(`Do you need to install any dependencies with NPM? (yes/no) \n`);
-    console.log('\n');
     const resp = await axios({
       method: 'get',
       url: `https://git.generalassemb.ly/api/v3/repos/${COHORT}/${REPO}/pulls`
     });
+    const NPM = prompt.question(`Do you need to install any dependencies with NPM? (yes/no) \n`);
+    console.log('\n');
     console.log(`cd .. && rm -rf ${REPO} && mkdir ${REPO} && cd ${REPO}`);
     const submits = resp.data.forEach(item => {
       let user = item.user.login;
