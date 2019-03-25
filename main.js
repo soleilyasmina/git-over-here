3dd90e17839c8e49fdc296be0b3a4d970aee807a
soleilyasmina <mounlasolomon@gmail.com>
2019-03-24 23:25:27 -0400

    big commit - added running loop - added colored text - fixed asynchronous output with prompt.question - v1 complete

--- main.js
+++ main.js
@@ -22,45 +22,59 @@ const colorText = (text, color) => {
 }
 
 const main = async () => {
-  try {
-    console.log(`echo Welcome to ${colorText('Git Over Here', 'blue')}!`);
-    const COHORT = prompt.question(`What is the name of your GitHub organization? (e.g. sei-nyc-jeopardy)\n`);
-    const REPO = prompt.question(`Which repository do you want to pull from? (e.g. js-data-types-homework)\n`);
-    const NPM = prompt.question(`Do you need to install any dependencies with NPM? (yes/no)\n`);
-    const resp = await axios({
-      method: 'get',
-      url: `https://git.generalassemb.ly/api/v3/repos/${COHORT}/${REPO}/pulls`
-    });
-    console.log(`echo ${colorText('Repo found!', 'green')}`);
-    console.log(`echo ${colorText(`${resp.data.length !== 1 ? resp.data.length + ' submissions' : '1 submission' } pulled.`, 'green')} Copying into ${colorText(`${REPO}`, 'blue')}.`);
-    console.log(`cd .. && rm -rf ${REPO} && mkdir ${REPO} && cd ${REPO}`);
-    const submits = resp.data.forEach(item => {
-      let user = item.user.login;
-      let title = item.title.split(' ')[0].replace(`'s`,'');
-      console.log(`git clone ${BASE_URL}${user}/${REPO} ${title}`);
-      console.log(`cd ${title} ${NPM === 'yes' ? '&& npm install' : ''} && cd ..`);
-    });
-    console.log(`echo ${colorText('Fetch complete!', 'green')}`);
-  } catch (e) {
-    let status;
-      try {
-        status = e.response.status;
-      } catch (e) {
-        status = undefined;
+  console.log(`echo Welcome to ${colorText('Git Over Here', 'blue')}!`);
+  let running = true;
+  while (running) {
+    try {
+      console.log(`echo What is the name of your ${colorText('GitHub organization', 'blue')}? e.g. ${colorText('sei-nyc-jeopardy','green')}\n`);
+      const COHORT = prompt.question();
+      console.log(`echo Which ${colorText('repository', 'blue')} do you want to pull from? e.g. ${colorText('js-data-types-homework', 'green')}\n`);
+      const REPO = prompt.question();
+            const resp = await axios({
+        method: 'get',
+        url: `https://git.generalassemb.ly/api/v3/repos/${COHORT}/${REPO}/pulls`
+      });
+      console.log(`echo ${colorText('Repo found!', 'green')}`);
+      console.log(`echo ${colorText(`${resp.data.length !== 1 ? resp.data.length + ' submissions' : '1 submission' } pulled.`, 'green')} Copying into ${colorText(`${REPO}`, 'blue')}.`);
+      console.log(`echo Do you need to install any dependencies with ${colorText('NPM', 'blue')}? ${colorText('yes', 'green')}/${colorText('no','red')}\n`);
+      const NPM = prompt.question();
+      console.log(`cd .. && rm -rf ${REPO} && mkdir ${REPO} && cd ${REPO}`);
+      const submits = resp.data.forEach(item => {
+        let user = item.user.login;
+        let title = item.title.split(' ')[0].replace(`'s`,'');
+        console.log(`git clone ${BASE_URL}${user}/${REPO} ${title}`);
+        console.log(`cd ${title} ${NPM === 'yes' ? '&& npm install' : ''} && cd ..`);
+      });
+      console.log(`echo ${colorText('Fetch complete!', 'green')}`);
+      running = false;
+    } catch (e) {
+      let status;
+        try {
+          status = e.response.status;
+        } catch (e) {
+          status = undefined;
+        }
+      switch(status) {
+        case 404:
+          console.log(`echo ${colorText('Sorry, this repo was not found.', 'red')}`);
+          break;
+        case undefined:
+          console.log(`echo ${colorText('Sorry, your connection was interrupted.', 'red')}`);
+          break;
+        default:
+          console.log('echo Error.');
+          console.log(`echo ${status}`)
+          break;
       }
-    switch(status) {
-      case 404:
-        console.log(`echo Sorry, this repo was ${colorText('not found', 'red')}!`);
-        break;
-      case undefined:
-        console.log(`echo Sorry, your connection was interrupted.`);
-        break;
-      default:
-        console.log('echo Error.');
-        console.log(`echo ${status}`)
-        break;
+      console.log(`echo Would you like to try again? yes/no\n`);
+      let tryAgain = prompt.question();
+      if (tryAgain !== 'yes') {
+        console.log(`echo Thank you for using ${colorText('Git Over Here', 'blue')}.`);
+        running = false;
+      }
     }
   }
+  process.exit();
 }
 
 main();
