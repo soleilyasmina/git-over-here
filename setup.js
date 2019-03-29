7c381900669bd3a834bfd4d2090db06de46ae840
soleilyasmina <mounlasolomon@gmail.com>
2019-03-29 14:00:51 -0400

    fixed token bug

--- setup.js
+++ setup.js
@@ -1,13 +1,20 @@
 const prompt = require('readline-sync');
 const { green, red, blue } = require('./style');
 
+require('dotenv').config();
+
 const setup = () => {
   console.log(`echo What is the name of your ${blue('current GitHub organization')}? e.g. ${green('sei-nyc-jeopardy')}`);
   const COHORT = prompt.question();
   console.log(`echo What would you like to be called by ${blue('Git Over Here')}?`);
   const COWPOKE = prompt.question();
-  console.log(`echo What is your ${blue('personal access token')}?\n`);
-  const TOKEN = prompt.question();
+  let TOKEN;
+  if (!process.env.TOKEN) {
+    console.log(`echo What is your ${blue('personal access token')}?\n`);
+    TOKEN = prompt.question();
+  } else {
+    TOKEN = process.env.TOKEN;
+  }
   let info = `\"COHORT=${COHORT}\nCOWPOKE=${COWPOKE}\nTOKEN=${TOKEN}\"`;
   console.log(`touch .env && echo ${info} > .env`);
   console.log(`echo ${green('Thank you!')}`);
