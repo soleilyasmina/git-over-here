fa88b393f8011a4651ed232f6d00c42ecd954861
soleilyasmina <mounlasolomon@gmail.com>
2019-07-22 15:54:33 -0400

    format

--- bundle.js
+++ bundle.js
@@ -3,20 +3,19 @@ const fs = require('file-system');
 const installDependencies = () => {
   const REPO = process.argv[2];
   const TITLE = process.argv[3];
-  fs.recurse(`../${REPO}/${TITLE}`, ['Gemfile','**/Gemfile'], 
+  fs.recurse(`../${REPO}/${TITLE}`, ['Gemfile', '**/Gemfile'],
     (filepath, relative, filename) => {
       if (filename === 'Gemfile') {
-        let folderDepth = filepath.split('/');
+        const folderDepth = filepath.split('/');
         folderDepth.pop();
-        console.log(`echo Installing dependencies for ${TITLE}.`)
-        if (folderDepth.length > 0) console.log(`cd ${filepath.replace('Gemfile','')}`);
-        console.log(`bundle install`);
+        console.log(`echo Installing dependencies for ${TITLE}.`);
+        if (folderDepth.length > 0) console.log(`cd ${filepath.replace('Gemfile', '')}`);
+        console.log('bundle install');
         for (let i = 0; i < folderDepth.length; i++) {
-          console.log(`cd ..`);
+          console.log('cd ..');
         }
       }
-    }
-  )
-}
+    });
+};
 
 installDependencies();
